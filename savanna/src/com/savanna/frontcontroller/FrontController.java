82a555c7c4a98fbc3e309e34ecd105ca65311474
범 <sazya24@gmail.com>
2021-02-22 01:29:47 +0900

    1
    1

--- savanna/src/com/savanna/frontcontroller/FrontController.java
+++ savanna/src/com/savanna/frontcontroller/FrontController.java
@@ -24,6 +24,13 @@ import com.savanna.model.command.book.UpdateBookCommand;
 import com.savanna.model.command.book.viewDispatchCommand.EnterBookDetailCommand;
 import com.savanna.model.command.book.viewDispatchCommand.EnterCreateBookViewCommand;
 import com.savanna.model.command.book.viewDispatchCommand.EnterUpdateBookViewCommand;
+import com.savanna.model.command.cart.CartInsertCommand;
+import com.savanna.model.command.cart.CartListCommand;
+import com.savanna.model.command.cart.ClearCartCommand;
+import com.savanna.model.command.cart.DeleteInCartCommand;
+import com.savanna.model.command.cart.EditQuantCommand;
+import com.savanna.model.command.cart.OrderGoCommand;
+import com.savanna.model.command.cart.TempBookInsertCommand;
 import com.savanna.model.command.member.AdminUpdateCommand;
 import com.savanna.model.command.member.AdminWithdrawalCommand;
 import com.savanna.model.command.member.DoPwdCheckCommand;
@@ -42,6 +49,10 @@ import com.savanna.model.command.member.SignInCommand;
 import com.savanna.model.command.member.SignUpCommand;
 import com.savanna.model.command.member.UpdateCommand;
 import com.savanna.model.command.member.WithdrawalCommand;
+import com.savanna.model.command.wish.DeleteWishCommand;
+import com.savanna.model.command.wish.InsertWishCommand;
+import com.savanna.model.command.wish.WishListCommand;
+import com.savanna.model.command.wish.WishitemToCartCommand;
 import com.savanna.model.command.cart.*;
 
 @WebServlet("/controller")
@@ -52,8 +63,6 @@ public class FrontController extends HttpServlet {
 		String type = request.getParameter("type");
 		String id = request.getParameter("id");
 		String pwd = request.getParameter("pwd");
-		
-		String cPage = request.getParameter("cPage"); // jsp에서 cPage 전달받아 동적검색할 command의 cPage로 넘김
 		System.out.println("> type : "+ type);
 
 		Command command = null;
@@ -77,7 +86,9 @@ public class FrontController extends HttpServlet {
 			command = new UpdateCommand();
 		} else if ("withdrawal".equals(type)) {
 			command = new WithdrawalCommand();
-		} else if (type.indexOf("memList") == 0) {
+		}
+
+		else if (type.indexOf("memList") == 0) {
 			command = type.length()==7
 					? new MemListCommand("1")
 					: new MemListCommand(type.substring(14));
@@ -98,7 +109,7 @@ public class FrontController extends HttpServlet {
 		} else if ("adminWithdrawal".equals(type)) {
 			command = new AdminWithdrawalCommand();
 		} else if ("memsearch".equals(type)) {
-			command = new MemSearchCommand(cPage);
+			command = new MemSearchCommand();
 		} else if("StockList".equals(type)) {
 			command = new EnterCardListCommand();
 		} else if("StockManagerPage".equals(type)) {
@@ -134,53 +145,45 @@ public class FrontController extends HttpServlet {
 			command = new CommInsertCommand();
 		} else if("commDelete".equals(type)) {
 			command = new CommDeleteCommand();
-		}
-		else if ("cartList".equals(type)) { //장바구니 보기
+		} else if ("cartList".equals(type)) { //장바구니 보기
 			command = new CartListCommand();
-		}
-		else if("cartInsert".equals(type)){ //장바구니 넣기
+		} else if("cartInsert".equals(type)){ //장바구니 넣기
 			command = new CartInsertCommand();
-		}
-		else if("deleteInCart".equals(type)){ //장바구니에서 삭제
+		} else if("deleteInCart".equals(type)){ //장바구니에서 삭제
 			command = new DeleteInCartCommand();
-		}
-		else if("clearCart".equals(type)){ //장바구니 비우기
+		} else if("clearCart".equals(type)){ //장바구니 비우기
 			command = new ClearCartCommand();
-		}
-		else if("orderPayment".equals(type)){ //구매step1
+		} else if("orderPayment".equals(type)){ //구매step1
 			command = new OrderPaymentCommand();
-		}
-		else if("GoOrder".equals(type)){ //구매step2
+		} else if("GoOrder".equals(type)){ //구매step2
 			command = new OrderGoCommand();
-		}
-		else if("editQuan".equals(type)){ //장바구니에서 수량변경
+		} else if("editQuan".equals(type)){ //장바구니에서 수량변경
 			command = new EditQuantCommand();
-		}
-		else if("tempBookInsert".equals(type)){ //Cart에 담기위한 임시상품리스트
+		} else if("tempBookInsert".equals(type)){ //Cart에 담기위한 임시상품리스트
 			command = new TempBookInsertCommand();
-		}
-		else if("paymentComplete".equals(type)){ //구매완료
+		} else if("paymentComplete".equals(type)){ //구매완료
 			command = new PaymentCompleteCommand();
-		}
-		else if("creditCardProc".equals(type)){ //카드결제진행창
+		} else if("creditCardProc".equals(type)){ //카드결제진행창
 			command = new CreditCardProcCommand();
-		}
-		else if("payCreditCard".equals(type)){ //카드결제완료
+		} else if("payCreditCard".equals(type)){ //카드결제완료
 			command = new PayCreditCardCommand();
 		} else if("MainPage".equals(type)) {
 			System.out.print("expect:MainPage");
 			command = new MainPageCommand();
-		} else if("MainPage".equals(type)) {
-			System.out.print("expect:MainPage");
-			command = new MainPageCommand();
-		} 
-		
-		
+		} else if("wishList".equals(type)) { //찜목록
+			command = new WishListCommand();
+		} else if("insertWishList".equals(type)) { //찜목록 입력
+			command = new InsertWishCommand();
+		} else if("deleteWishList".equals(type)) { //찜목록에서 삭제
+			command = new DeleteWishCommand();
+		} else if("wishitemToCart".equals(type)) { //장바구니로 이동(찜목록에서 삭제)
+			command = new WishitemToCartCommand();
+		}
 		else {
 			System.out.print("Command Error");
 		}
-		String path = command.execute(request, response);
 
+		String path = command.execute(request, response);
 
 		if(type.indexOf("List") != 0) {
 			request.setAttribute("curList", type);

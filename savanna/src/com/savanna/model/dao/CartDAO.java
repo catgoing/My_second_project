94f655fc2cd200f189f50aa824b5479629809f22
범 <sazya24@gmail.com>
2021-02-22 11:30:00 +0900

    da
    da

--- savanna/src/com/savanna/model/dao/CartDAO.java
+++ savanna/src/com/savanna/model/dao/CartDAO.java
@@ -11,7 +11,7 @@ import com.savanna.model.vo.CartVO;
 import com.savanna.model.vo.MemberVO;
 import com.savanna.mybatis.DBService;
 
-public class CartDAO {
+public class CartDAO implements SuperDAO{
 
 	// 장바구니 추가 - 매개변수 : 책번호, 회원id
 	public static boolean insertCart(int book_no, String id) {
